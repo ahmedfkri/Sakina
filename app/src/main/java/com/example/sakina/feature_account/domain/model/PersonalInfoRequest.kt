e8783075992a0d9700fdab7aeb0286c0039416ca
Ahmed Fkri <ahmedfkri@hotmail.com>
2024-03-13 17:05:35 +0200

    medical info logic

--- app/src/main/java/com/example/sakina/feature_account/domain/model/PersonalInfoRequest.kt
+++ app/src/main/java/com/example/sakina/feature_account/domain/model/PersonalInfoRequest.kt
@@ -4,8 +4,9 @@ data class PersonalInfoRequest(
     val age: Int,
     val height: Int,
     val weight: Int,
-    val diabetic: Boolean,
-    val hypertension: Boolean,
-    val hypotension: Boolean,
-    val smoker: Boolean
+    var diabetic: Boolean,
+    var hypertension: Boolean,
+    var hypotension: Boolean,
+    var smoker: Boolean
 )
+
